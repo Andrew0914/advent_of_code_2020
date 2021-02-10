9e6b41b7865a64c2298d5c55d53732d540c0ab49
Andrew González <andrewalangm@gmail.com>
2021-02-10 16:24:41 -0600

    📝 Day 1 - expense report

---
+++ typescript/src/index.ts
@@ -0,0 +1,4 @@
+import { findProduct, getProductTrioToSum } from './puzzle1';
+// Results
+console.log('Puzzle 1 | PART 1📝: ', findProduct());
+console.log('Puzzle 1 | PART 2📝: ', getProductTrioToSum(2020));
