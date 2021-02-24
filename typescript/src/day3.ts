cd39e748ed021dff25bef63005356a7c330504cd
Andrew0914 <andrewalangm@gmail.com>
2021-02-23 20:57:21 -0600

    🥪 Solution day 3 part 2

--- typescript/src/day3.ts
+++ typescript/src/day3.ts
@@ -1,27 +1,47 @@
 import { FileReader } from './FileReader';
 import { InputParser } from './InputParser';
 
-function encounterTree(square: string): number {
-  return square === "#" ? 1 : 0
-}
-
-function countTrees(forest: Array<string>, stepsX: number, stepsY: number): number {
-  let countTrees = 0;
-  let x = 0
-  for (let y = stepsY; y < forest.length; y += stepsY) {
-    x += stepsX
-    if (x  >= forest[y].length - 1)
-      x = x - (forest[y].length - 1)    
-    countTrees += encounterTree(forest[y].charAt(x))
-  }
-  return countTrees;
-}
-
 interface Steps {
   stepsX: number, stepsY: number
 }
-export function howManyTrees(foresFile: string, steps: Steps): number {
-  const reader = new FileReader(foresFile);
-  const forest = new InputParser(reader.getContent()).getLines();
-  return countTrees(forest, steps.stepsX, steps.stepsY);
+
+export class TreeFinder {
+  private forest: Array<string>
+
+  constructor(forestFile: string) {
+    this.forest = this.getForest(forestFile)
+  }
+
+  private getForest(foresFile: string) {
+    const reader = new FileReader(foresFile);
+    return new InputParser(reader.getContent()).getLines();
+  }
+
+  private encounterTree(square: string): number {
+    return square === "#" ? 1 : 0
+  }
+
+  private countTrees(forest: Array<string>, stepsX: number, stepsY: number): number {
+    let countTrees = 0;
+    let x = 0
+    for (let y = stepsY; y < forest.length; y += stepsY) {
+      x += stepsX
+      if (x >= forest[y].length - 1)
+        x = x % (forest[y].length - 1)
+      countTrees += this.encounterTree(forest[y].charAt(x))
+    }
+    return countTrees;
+  }
+
+  public howManyTrees(steps: Steps): number {
+    return this.countTrees(this.forest, steps.stepsX, steps.stepsY);
+  }
+
+  public bulkFind(stepsList: Array<Steps>): Array<number> {
+    const treecounter: Array<number> = new Array()
+    stepsList.forEach(steps => {
+      treecounter.push(this.howManyTrees(steps))
+    })
+    return treecounter
+  }
 }
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,7 +1,15 @@
 import { findProduct, getProductTrioToSum } from './day1';
-import { howManyTrees } from './day3';
-const FOREST_FILE = './src/forest.txt'
-// Results
+import { TreeFinder } from './day3';
+//DAY1
 console.log('Day 1 | PART 1📝: ', findProduct());
 console.log('Day 1 | PART 2📝: ', getProductTrioToSum(2020));
-console.log('Day 3 | PART 1 🌳:', howManyTrees(FOREST_FILE, { stepsX: 3, stepsY: 1 }));
+//DAY 3
+const FOREST_FILE = './src/forest.txt'
+const treeFinder = new TreeFinder(FOREST_FILE)
+console.log('Day 3 | PART 1 🌳:', treeFinder.howManyTrees({ stepsX: 3, stepsY: 1 }));
+const stepsList = [
+    { stepsX: 1, stepsY: 1 }, { stepsX: 3, stepsY: 1 },
+    { stepsX: 5, stepsY: 1 }, { stepsX: 7, stepsY: 1 },
+    { stepsX: 1, stepsY: 2 }
+]
+console.log('Day 3 | PART 2 🌳:', treeFinder.bulkFind(stepsList).reduce((acc, trees) => acc *= trees));
