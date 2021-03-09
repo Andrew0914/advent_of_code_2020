4a27d572ff789b16ec9c89128bf38878d678c82d
Andrew González <andrewalangm@gmail.com>
2021-03-09 14:36:47 -0600

    ✅ Day 6 - Part 2 sum of answers that response everyone

--- elixir/day6.ex
+++ elixir/day6.ex
@@ -10,16 +10,49 @@ defmodule CustomDeclarationForms do
   end
 
   defp parse_group_answers(group_answers) do
-    group_answers
-    |> String.split("\n")
-    |> Enum.join("")
-    |> String.graphemes()
+    individual_answers =
+      group_answers
+      |> String.split("\n")
+
+    answers =
+      individual_answers
+      |> Enum.join("")
+      |> String.graphemes()
+
+    %{participants: length(individual_answers), answers: answers}
   end
 
-  def sum_of_yes_answers(file_path) do
+  def get_answers_ocurrences(answers) do
+    ocurrences =
+      Map.get(answers, :answers)
+      |> Enum.sort()
+      |> Enum.reduce(%{}, fn answer, acc ->
+        Map.update(acc, answer, 1, fn ocurrences -> ocurrences + 1 end)
+      end)
+
+    %{participants: Map.get(answers, :participants), ocurrences: ocurrences}
+  end
+
+  def sum_of_yes_answers_from_anyone(file_path) do
     get_forsm_file_content(file_path)
     |> get_forms_answers()
-    |> Enum.map(fn answers -> Enum.uniq(answers) end)
+    |> Enum.map(fn answers -> Enum.uniq(Map.get(answers, :answers)) end)
     |> Enum.reduce(0, fn diff_answers, acc -> acc + length(diff_answers) end)
+  end
+
+  def sum_of_yes_answers_from_everyone(file_path) do
+    get_forsm_file_content(file_path)
+    |> get_forms_answers()
+    |> Enum.map(fn answers -> get_answers_ocurrences(answers) end)
+    |> Enum.reduce(0, fn answers_ocurrences, acc ->
+      x =
+        Map.get(answers_ocurrences, :ocurrences)
+        |> Map.values()
+        |> Enum.count(fn ocurrence ->
+          ocurrence == Map.get(answers_ocurrences, :participants)
+        end)
+
+      acc + x
+    end)
   end
 end
