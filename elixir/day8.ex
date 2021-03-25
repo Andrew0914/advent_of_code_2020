bfe9aaf8a1ac3f1198152bc3571428ea17393876
Andrew González <andrewalangm@gmail.com>
2021-03-25 10:46:44 -0600

    🐞 Day 8 - Part 2 Finding and fix bug to get finall accumulator

--- elixir/day8.ex
+++ elixir/day8.ex
@@ -16,35 +16,72 @@ defmodule GameBootFixing do
     |> Enum.map(&parse_instruction/1)
   end
 
-  defp execute({"acc", number, count, index}) do
-    {index + 1, count + number}
+  defp execute({"acc", number, accumulator, index}) do
+    {index + 1, accumulator + number}
   end
 
-  defp execute({"jmp", number, count, index}) do
-    {index + number, count}
+  defp execute({"jmp", number, accumulator, index}) do
+    {index + number, accumulator}
   end
 
-  defp execute({"nop", _, count, index}) do
-    {index + 1, count}
+  defp execute({"nop", _, accumulator, index}) do
+    {index + 1, accumulator}
   end
 
-  defp get_count(code_instructions, history, {index, count}) do
-    [txt_instruction, number] = Enum.at(code_instructions, index)
-
-    if Enum.count(history, &(&1 == {txt_instruction, index})) >= 1 do
-      count
+  defp get_accumulator(code_instructions, history, {index, accumulator}) do
+    if index >= length(code_instructions) do
+      {:terminated, accumulator}
     else
-      get_count(
-        code_instructions,
-        history ++ [{txt_instruction, index}],
-        execute({txt_instruction, number, count, index})
-      )
+      [txt_instruction, number] = Enum.at(code_instructions, index)
+
+      if Enum.count(history, &(&1 == {txt_instruction, index})) >= 1 do
+        {:loop, accumulator}
+      else
+        get_accumulator(
+          code_instructions,
+          history ++ [{txt_instruction, index}],
+          execute({txt_instruction, number, accumulator, index})
+        )
+      end
     end
   end
 
-  def get_count_before_infitine_loop(code_file_path) do
+  def get_accumulator_before_infitine_loop(code_file_path) do
     get_code_content(code_file_path)
     |> get_code_instructions()
-    |> get_count([], {0, 0})
+    |> get_accumulator([], {0, 0})
+  end
+
+  # ----- PART 2 -----
+
+  defp fix(bug_instruction) do
+    [txt_instruction, number] = bug_instruction
+
+    case txt_instruction do
+      "jmp" -> ["nop", number]
+      "nop" -> ["jmp", number]
+    end
+  end
+
+  def get_accumulator_after_fixing_bug(code_file_path) do
+    code_instructions =
+      get_code_content(code_file_path)
+      |> get_code_instructions()
+
+    code_instructions
+    |> Enum.with_index()
+    |> Enum.filter(fn {instruction, _} ->
+      [txt_instruction, _] = instruction
+      txt_instruction == "jmp" or txt_instruction == "nop"
+    end)
+    |> Enum.reduce_while(0, fn bug, acc ->
+      {bug_instruction, index} = bug
+
+      acc_status =
+        List.replace_at(code_instructions, index, fix(bug_instruction))
+        |> get_accumulator([], {0, 0})
+
+      if elem(acc_status, 0) == :loop, do: {:cont, 0}, else: {:halt, acc_status}
+    end)
   end
 end
