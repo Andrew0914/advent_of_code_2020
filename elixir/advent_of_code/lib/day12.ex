e9b1c17bc4998bd75abc1b878e89d555156622c7
AndrewG <andrewalangm@gmail.com>
2021-05-26 17:59:41 -0500

    ⛵ Day 122 Navigation system -  part 1

---
+++ elixir/advent_of_code/lib/day12.ex
@@ -0,0 +1,96 @@
+defmodule NavigationSystem do
+  defp get_name_origin(key_location) do
+    number_location =
+      if key_location <= 0,
+        do: key_location + 1,
+        else: key_location
+
+    %{1.0 => "N", 0.25 => "E", 0.5 => "S", 0.75 => "W"}
+    |> Map.get(number_location)
+  end
+
+  defp get_number_origin(origin) do
+    %{"N" => 1.0, "E" => 0.25, "S" => 0.5, "W" => 0.75}
+    |> Map.get(origin)
+  end
+
+  def where_it_does_turn(origin, "L", degrees) do
+    (get_number_origin(origin) + (1 - degrees / 360) - 1)
+    |> get_name_origin()
+  end
+
+  def where_it_does_turn(origin, "R", degrees) do
+    (get_number_origin(origin) + degrees / 360 - 1)
+    |> get_name_origin()
+  end
+
+  def do_instruction(current_points, "R", degrees) do
+    new_direction = Map.get(current_points, "direction") |> where_it_does_turn("R", degrees)
+
+    current_points
+    |> Map.put("direction", new_direction)
+  end
+
+  def do_instruction(current_points, "L", degrees) do
+    new_direction = Map.get(current_points, "direction") |> where_it_does_turn("L", degrees)
+
+    current_points
+    |> Map.put("direction", new_direction)
+  end
+
+  def do_instruction(current_points, "F", distance) do
+    current_direction = Map.get(current_points, "direction")
+    current_distance = Map.get(current_points, current_direction)
+
+    current_points
+    |> Map.put(current_direction, current_distance + distance)
+  end
+
+  def do_instruction(current_points, cardinal_point, distance) do
+    current_points
+    |> Map.put(cardinal_point, Map.get(current_points, cardinal_point) + distance)
+  end
+
+  defp update_index(navigation) do
+    navigation |> Map.put("index", Map.get(navigation, "index") + 1)
+  end
+
+  def navigate(navigation, instructions) do
+    if navigation["index"] >= length(instructions) do
+      navigation
+    else
+      {action, value} = instructions |> Enum.at(navigation["index"])
+
+      do_instruction(navigation, action, value)
+      |> update_index()
+      |> navigate(instructions)
+    end
+  end
+
+  def calculate_distance(navigation) do
+    abs(navigation["E"] - navigation["W"]) + abs(navigation["N"] - navigation["S"])
+  end
+
+  def parse_instruction(instruction) do
+    action = instruction |> String.at(0)
+    value = instruction |> String.slice(1, String.length(instruction) - 1) |> String.to_integer()
+    {action, value}
+  end
+
+  def solution_one(file_path) do
+    initial_navigation = %{
+      "index" => 0,
+      "E" => 0,
+      "W" => 0,
+      "N" => 0,
+      "S" => 0,
+      "direction" => "E"
+    }
+
+    instructions =
+      File.read!(file_path)
+      |> String.split(~r/\r\n|\n/)
+      |> Enum.map(&parse_instruction/1)
+    initial_navigation |> navigate(instructions) |> calculate_distance()
+  end
+end
---
+++ elixir/advent_of_code/test/day12_test.exs
@@ -0,0 +1,145 @@
+defmodule NavigationSystemTest do
+  use ExUnit.Case, async: true
+  use ExUnit.Parameterized
+
+  test_with_params "Determine position after turning left",
+    fn origin, degrees, expected ->
+      assert NavigationSystem.where_it_does_turn(origin, "L", degrees) == expected
+    end
+    do
+    [
+      {"E", 90, "N"},
+      {"E", 180, "W"},
+      {"E", 270, "S"},
+      {"W", 90, "S"},
+      {"W", 180, "E"},
+      {"W", 270, "N"},
+      {"N", 90, "W"},
+      {"N", 180, "S"},
+      {"N", 270, "E"},
+      {"S", 90, "E"},
+      {"S", 180, "N"},
+      {"S", 270, "W"}
+    ]
+  end
+
+  test_with_params "Determine position after turning right",
+    fn origin, degrees, expected ->
+      assert NavigationSystem.where_it_does_turn(origin, "R", degrees) == expected
+    end
+    do
+    [
+      {"E", 90, "S"},
+      {"E", 180, "W"},
+      {"E", 270, "N"},
+      {"W", 90, "N"},
+      {"W", 180, "E"},
+      {"W", 270, "S"},
+      {"N", 90, "E"},
+      {"N", 180, "S"},
+      {"N", 270, "W"},
+      {"S", 90, "W"},
+      {"S", 180, "N"},
+      {"S", 270, "E"}
+    ]
+  end
+  # N S E W
+  test_with_params "Move to certain cardinal point",
+    fn current_points, cardinal_point, distance, expected ->
+      assert NavigationSystem.do_instruction(
+              current_points,
+              cardinal_point,
+              distance
+            ) == expected
+    end
+    do
+    [
+      {%{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "E"}, "E", 10,
+       %{"index" => 0, "E" => 35, "W" => 0, "N" => 100, "S" => 90, "direction" => "E"}},
+      {%{"index" => 0, "E" => 25, "W" => 20, "N" => 100, "S" => 90, "direction" => "E"}, "W", 20,
+       %{"index" => 0, "E" => 25, "W" => 40, "N" => 100, "S" => 90, "direction" => "E"}},
+      {%{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "E"}, "N", 25,
+       %{"index" => 0, "E" => 25, "W" => 0, "N" => 125, "S" => 90, "direction" => "E"}},
+      {%{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "E"}, "S", 30,
+       %{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 120, "direction" => "E"}}
+    ]
+  end
+
+  # R - L
+  test_with_params "turn (update) the cardinal point we have to go",
+    fn current_points, turn_direction, degrees, expected ->
+      assert NavigationSystem.do_instruction(
+              current_points,
+              turn_direction,
+              degrees
+            ) == expected
+    end
+    do
+    [
+      {%{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "E"}, "L", 90,
+       %{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "N"}},
+      {%{"index" => 0, "E" => 25, "W" => 20, "N" => 100, "S" => 90, "direction" => "N"}, "R", 180,
+       %{"index" => 0, "E" => 25, "W" => 20, "N" => 100, "S" => 90, "direction" => "S"}},
+      {%{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "S"}, "L", 270,
+       %{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "W"}},
+      {%{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "W"}, "R", 90,
+       %{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "N"}}
+    ]
+  end
+
+  test_with_params "Go forward to the right direction",
+    fn current_points, go_forward, distance, expected ->
+      assert NavigationSystem.do_instruction(
+              current_points,
+              go_forward,
+              distance
+            ) == expected
+    end
+    do
+    [
+      {%{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "E"}, "F", 90,
+       %{"index" => 0, "E" => 115, "W" => 0, "N" => 100, "S" => 90, "direction" => "E"}},
+      {%{"index" => 0, "E" => 25, "W" => 20, "N" => 280, "S" => 90, "direction" => "N"}, "F", 180,
+       %{"index" => 0, "E" => 25, "W" => 20, "N" => 460, "S" => 90, "direction" => "N"}},
+      {%{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "S"}, "F", 270,
+       %{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 360, "direction" => "S"}},
+      {%{"index" => 0, "E" => 25, "W" => 0, "N" => 100, "S" => 90, "direction" => "W"}, "F", 90,
+       %{"index" => 0, "E" => 25, "W" => 90, "N" => 100, "S" => 90, "direction" => "W"}}
+    ]
+  end
+
+  # TODO Create method to satisfy this test
+  test "Perform all instructions gets navigation info" do
+    # Arrange
+    instructions = [{"F", 10}, {"N", 3}, {"F", 7}, {"R", 90}, {"F",11}]
+    initial_navigation = %{"index" => 0, "E" => 0, "W" => 0, "N" => 0, "S" => 0, "direction" => "E"}
+    expected_navigation = %{"index" => 5, "E" => 17, "W" => 0, "N" => 3, "S" => 11, "direction" => "S"}
+
+    # Act
+    navigation = NavigationSystem.navigate(initial_navigation, instructions)
+
+    # Assert
+    assert navigation == expected_navigation
+  end
+
+  test "Calculate distance with navigation system" do
+    #Arrange
+    navigation = %{"index" => 5, "E" => 17, "W" => 0, "N" => 3, "S" => 11, "direction" => "S"}
+    expected_distance = 25
+    #Act
+    distance = navigation |> NavigationSystem.calculate_distance()
+    # Assert
+    assert distance == expected_distance
+  end
+
+  test "Parse string instruction into manageable tuple" do
+    # Arrange
+    str_instruction = "N10"
+    expected_tuple = {"N", 10}
+    # Act
+    tuple_instruction = NavigationSystem.parse_instruction(str_instruction)
+    # Assert
+    assert tuple_instruction == expected_tuple
+  end
+
+end
